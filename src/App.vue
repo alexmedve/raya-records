04afd39169bd8cd336802c3aea2966c0d5a88be6
alexmedve <41807747+alexmedve@users.noreply.github.com>
2020-12-10 17:59:40 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+	<router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+//HELPER CLASSES
+.u-text-align-right {
+  text-align: right;
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home u-text-align-right">
+	home
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss">
+.home {
+  color: $test;
+}
+</style>
